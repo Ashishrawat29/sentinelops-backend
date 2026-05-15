524d8c982079f9a6882a3efa94df1242ad4d803f
Ashish Rawat <rawatashish96304@gmail.com>
2026-05-16 04:12:20 +0530

    2.Added firebase admin dependency

--- app/schemas/prediction_schema.py
+++ app/schemas/prediction_schema.py
@@ -1,8 +1,16 @@
 from pydantic import BaseModel
 
-class PredictionInput(BaseModel):
+
+class PredictionInput(
+    BaseModel,
+):
+
     temperature: float
+
     vibration: float
+
     voltage: float
+
     pressure: float
+
     runtime_hours: float
